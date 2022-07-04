a3cd2c3bccdb11ef671cfa08d8218df06e5e340d
Hô Văn Toàn <it.vantoan@gmail.com>
2022-07-04 14:57:00 +0700

    add content

---
+++ ToolSeoViet.Database/Models/SubTitle.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolSeoViet.Database.Models {
+    public partial class SubTitle {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string TitleId { get; set; }
+        public Title Title { get; set; }
+    }
+
+    public class SubTitleConfig : IEntityTypeConfiguration<SubTitle> {
+
+        public void Configure(EntityTypeBuilder<SubTitle> builder) {
+            builder.ToTable(nameof(SubTitle));
+
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).HasMaxLength(32);
+            builder.Property(o => o.TitleId).HasMaxLength(32);
+            builder.Property(o => o.Name).HasMaxLength(255).IsRequired();
+            // fk
+            builder.HasOne(o => o.Title).WithMany(o => o.SubTitles).HasForeignKey(o => o.TitleId);
+
+        }
+    }
+}
---
+++ ToolSeoViet.Database/Models/Title.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolSeoViet.Database.Models {
+    public partial class Title {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string HeadingId { get; set; }
+        public Heading Heading { get; set; }
+
+        public virtual ICollection<SubTitle> SubTitles { get; set; }
+    }
+    public class TitleConfig : IEntityTypeConfiguration<Title> {
+
+        public void Configure(EntityTypeBuilder<Title> builder) {
+            builder.ToTable(nameof(User));
+
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).HasMaxLength(32);
+            builder.Property(o => o.HeadingId).HasMaxLength(32).IsRequired();
+            builder.Property(o => o.Name).HasMaxLength(32);
+
+
+            // fk
+            builder.HasOne(o => o.Heading).WithMany(o => o.Titles).HasForeignKey(o => o.HeadingId);
+
+        }
+    }
+}
