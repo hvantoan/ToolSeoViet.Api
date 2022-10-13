c9adc86cbd2a280e9bd4fe0226d78bfb6c89ef91
Hô Văn Toàn <it.vantoan@gmail.com>
2022-10-13 10:01:23 +0700

    Update

--- ToolSeoViet.Service/Enums.cs
+++ ToolSeoViet.Service/Enums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToolSeoViet.Service {
+﻿namespace ToolSeoViet.Service {
     public class Enums {
 
         public enum ECheckIndex {
